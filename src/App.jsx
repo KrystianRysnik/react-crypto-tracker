cf1cd8477034660f1ebd2123a8ed3ef1839f6442
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2022-07-05 18:41:08 +0200

    save in localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import CryptoList from "./components/CryptoList";
-import initialStatecryptocurrencies from "./data";
 import cryptocurrencyReducer from "./reducers/cryptocurrencyReducer";
 
 function App() {
   const [cryptocurrencies, dispatchCryptocurrencies] = useReducer(
     cryptocurrencyReducer,
-    initialStatecryptocurrencies
+    JSON.parse(localStorage.getItem("cryptocurrencies")) || []
   );
+
+  useEffect(() => {
+    const json = JSON.stringify(cryptocurrencies);
+    localStorage.setItem("cryptocurrencies", json);
+  });
 
   return (
     <div className="h-full p-4 bg-slate-200">
